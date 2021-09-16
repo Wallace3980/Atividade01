d641df9b26c66347065e19347c6bb7e7047a7744
Wallace Rodrigues <aleksander.rodrigues@estudante.ifms.edu.br>
2021-09-16 10:45:43 -0300

    Tarefa Commit da Tela de Login

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
-//
+// "terminado"
 
 export default function App() {
     return(
